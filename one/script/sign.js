5141ab7d948f67cec4e3c329361024b30b45cc75
leihongyan <1527220895@qq.com>
2017-03-20 18:23:57 +0800

    3-20

---
+++ one/script/sign.js
@@ -0,0 +1,50 @@
+function delWord(el) {
+    var input = $api.prev(el, '.txt');
+    input.value = '';
+}
+
+function login() {
+    api.openWin({
+        name: 'login',
+        url: 'login.html',
+        opaque: true,
+        vScrollBarEnabled: false
+    });
+}
+
+function ensure() {
+    api.showProgress({
+        title: '正在登录...',
+        modal: false
+    });
+    var name = $api.byId('username').value;
+    var pwd = $api.byId('password').value;
+
+    var loginUlr = '/user/login';
+    var bodyParam = {
+        username: name,
+        password: pwd
+    }
+    ajaxRequest(loginUlr, 'post', JSON.stringify(bodyParam), function (ret, err) {
+        if (ret) {
+             $api.setStorage('userid', ret.userId);       	
+         
+            setTimeout(function () {
+
+                api.closeWin();
+                
+            }, 100);
+        } else {
+            api.alert({
+                msg: '登录不成功'
+            });
+        }
+        api.hideProgress();
+    })
+}
+
+apiready = function () {
+    var header = $api.byId('header');
+    $api.fixIos7Bar(header);
+
+};
